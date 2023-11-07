048d58e64f30b53ed525323f273060a9e9c280b2
Unlucked01 <113236909+Unlucked01@users.noreply.github.com>
2023-11-07 22:11:08 +0300

    added test

--- bot/test.py
+++ bot/test.py
@@ -1,4 +1,6 @@
 import asyncio
+import re
+
 import dotenv
 import os
 
@@ -15,41 +17,74 @@ bot = Bot(token=os.getenv('BOT_TOKEN'))
 dp = Dispatcher(bot)
 
 
+async def check_connection():
+    try:
+        return await bot.get_chat(chat_id=os.getenv('PRIVATE_CHAT_ID'))
+    except Exception as e:
+        print(f"Bot {bot.id} is not in chat: {e}")
+
+
+async def check_permission():
+    try:
+        bot_status = await bot.get_chat_member(chat_id=os.getenv('PRIVATE_CHAT_ID'), user_id=bot.id)
+        return bot_status.status == 'administrator'
+    except Exception as e:
+        print(f"Bot {bot.id} has no required status, current is {bot_status.status}: {e}")
+
+
+async def get_chat_name(chat_id):
+    await check_connection()
+    chat_info = await bot.get_chat(chat_id=os.getenv('PRIVATE_CHAT_ID'))
+    return chat_info.username
+
+
+async def bot_send_message(message_text):
+    await bot.send_message(chat_id=os.getenv('PRIVATE_CHAT_ID'), text=message_text, parse_mode='HTML')
+    await asyncio.sleep(10)
+
+
 async def main():
     client = TelegramClient('your_session', int(api_id), api_hash)
     await client.start()
 
-    keywords = ["нет ссылки", "зум", "задачка"]
+    keywords = ["тех задание", "зум", "задачка"]
 
     def find_keywords(message):
-        found_keyword = [keyword for keyword in keywords if keyword.lower() in message.text.lower()]
-        return found_keyword
+        for keyword in keywords:
+            if keyword.lower() in message.message.lower():
+                return message.message.lower()
+        return None
+
+        # return found_keywords
 
     chats = await client.get_dialogs()
     for chat in chats:
         if chat.title == "Avatarex Dev":
             messages = await client.get_messages(entity=chat, limit=100)
             for message in messages:
-                if message:
-                    try:
-                        found_keywords = find_keywords(message)
-                        if found_keywords:
-                            keyword_str = ", ".join(found_keywords)
-                            message_text = f"{message.text}\n\n"
-                            message_text += f"Пользователь: ({message.sender.username}), {message.sender.first_name}\nГруппа: "
-                            message_text += f"<a href='https://t.me/{chat.title.replace(' ', '_')}'>{chat.title}</a>\n"
-                            message_text += f"Ключ: {keyword_str}\n"
-                            message_text += f"<a href='https://t.me/{chat.title.replace(' ', '_')}/{message.id}'>Оригинал сообщения</a>"
+                try:
+                    found_keywords = find_keywords(message)
+                    print(found_keywords)
+                    if found_keywords:
+                        keyword_str = ", ".join(found_keywords)
+                        message_text = f"{message.text}\n\n"
+                        message_text += f"Пользователь: ({message.sender.username}), {message.sender.first_name}\nГруппа: "
+                        message_text += f"<a href='https://t.me/{chat.title.replace(' ', '_')}'>{chat.title}</a>\n"
+                        message_text += f"Ключ: {keyword_str}\n"
+                        message_text += f"<a href='https://t.me/{chat.title.replace(' ', '_')}/{message.id}'>Оригинал сообщения</a>"
 
-                            await bot.send_message(chat_id=os.getenv('PRIVATE_CHAT_ID'), text=message_text,
-                                                   parse_mode='HTML')
-                            await asyncio.sleep(10)
-                    except Exception as e:
-                        print(f"Error processing {chat.title}: {e}")
+                        if await check_connection():
+                            print("connected!")
+                            if await check_permission():
+                                print("permission granted!")
+                                # await bot_send_message(message_text)
+                                print("message sended!")
+
+                except Exception as e:
+                    print(f"Error processing {chat.title}: {e}")
 
 
 if __name__ == '__main__':
-    # Run the Telethon and Aiogram main functions
     from aiogram import executor as ag_executor
 
     ag_executor.start(dp, main())
